da17a194554af722a1ed025f64325df1fea2add2
iseryahel <yahel.iser@post.runi.ac.il>
2025-11-14 17:36:59 +0200

    check

--- LoanCalc.java
+++ LoanCalc.java
@@ -68,7 +68,7 @@ public class LoanCalc {
 			double mid = (low + high) / 2.0;
         	double fMid = endBalance(loan, rate, n, mid);
 			iterationCounter++;	
-			if ( Flow * Fhigh > 0)
+			if ( Flow * fMid > 0)
 			{
 				low = mid;
 				Flow= fMid;
